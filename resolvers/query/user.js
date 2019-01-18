aa3b4d24ebb001efd3dd8338b1af423a40fd5507
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-18 11:07:23 -0700

    added Goal to schema

--- resolvers/query/user.js
+++ resolvers/query/user.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const knex = require('../../knex');
 
 module.exports = async (root, args) => {
-  const user = await knex.select('username', 'id')
+  const user = await knex.select('username', 'id', 'email')
     .from('users')
     .where({id: args.id})
     .first();
