5612174d710ab3f7a61a6c903d5874d30a63132a
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-18 10:57:40 -0700

    added query for single user

--- resolvers/query/user.js
+++ resolvers/query/user.js
@@ -2,11 +2,9 @@ const axios = require('axios');
 const knex = require('../../knex');
 
 module.exports = async (root, args) => {
-  return await knex.select(*)
-  .from('users')
-  .then(results => {
-    console.log(`Here are the results: ${JSON.stringify(results)}`);
-    return JSON.stringify(results);
-  })
-  .catch(err => err);
+  const user = await knex.select('username', 'id')
+    .from('users')
+    .where({id: args.id})
+    .first();
+  return user;  
 }
