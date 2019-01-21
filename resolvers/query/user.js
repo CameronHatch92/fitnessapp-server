9567aa42c9907054ccb57b5d76b5797c880ceec7
Cameron Hatch <cameronlavigne92@gmail.com>
2019-01-21 12:57:21 -0700

    updated user query to include goals

--- resolvers/query/user.js
+++ resolvers/query/user.js
@@ -2,9 +2,18 @@ const axios = require('axios');
 const knex = require('../../knex');
 
 module.exports = async (root, args) => {
+
   const user = await knex.select('username', 'id', 'email')
     .from('users')
     .where({id: args.id})
     .first();
+
+  const goals = await knex.select('title', 'category', 'start_date', 'end_date',
+    'goal', 'complete', 'id', 'unit', 'type')
+    .from('goals')
+    .where({'user_id': args.id});
+  console.log(user);  
+  user.goals = goals;
+  console.log(user);  
   return user;  
 }
